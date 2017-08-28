5a6ea74704e66220f4968863042ac184292f6e49
Eric Schles <ericschles@gmail.com>
2017-08-28 13:43:12 -0400

    adding pshtt command

--- domain-scan/domain_scan/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-echo "running linters and tests..."
-flake8 . && python -m unittest discover tests
--- services/python_services/app.py
+++ services/python_services/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import json
 import subprocess
 from web_design_standards_check import uswds_checker
+
 app = Flask(__name__)
 
 
@@ -18,8 +19,13 @@ def services():
 
 @app.route("/services/pshtt", methods=["GET","POST"])
 def pshtt():
-    return "not done yet"
+    result = subprocess.run(["pshtt", "whitehouse.gov"], stdout=subprocess.PIPE)
+    return result.stdout
 
+@app.route("/services/command_test", methods=["GET","POST"])
+def command_test():
+    result = subprocess.run(["test_command"], stdout=subprocess.PIPE)
+    return result.stdout
 
 if __name__ == '__main__':
     app.run()
